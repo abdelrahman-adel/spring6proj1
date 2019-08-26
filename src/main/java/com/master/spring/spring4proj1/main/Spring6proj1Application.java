19fb32c18f271ac64dd6d840fa5e9d2a14c92f38
Abd-Elrahman Adel <abdelrahman-adel@hotmail.com>
2019-08-26 23:02:01 +0200

    Initial commit

---
+++ src/main/java/com/master/spring/spring4proj1/main/Spring6proj1Application.java
@@ -0,0 +1,13 @@
+package com.master.spring.spring4proj1.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6proj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6proj1Application.class, args);
+	}
+
+}
